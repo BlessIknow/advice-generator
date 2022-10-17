ebb1888415f1e7f55014b192ad3ec752bc7b826a
Blessing <blessvic2013@gmail.com>
2022-10-17 22:50:42 +0300

    Initial commit

---
+++ src/components/adviceGen.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class Generator extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment >
+        <div className="h-50 w-50 mx-auto d-block bg-primary p-3 mt-5">
+          <h1>hello</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Generator;
