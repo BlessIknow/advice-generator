4cd1e091965f659f6d5bbb4825cf56347d39cfb3
Blessing <blessvic2013@gmail.com>
2022-10-17 22:56:07 +0300

    first update

--- src/components/adviceGen.jsx
+++ src/components/adviceGen.jsx
@@ -3,8 +3,9 @@ class Generator extends Component {
   state = {};
   render() {
     return (
-      <React.Fragment >
+      <React.Fragment>
         <div className="h-50 w-50 mx-auto d-block bg-primary p-3 mt-5">
+          <h1>hello</h1>
           <h1>hello</h1>
         </div>
       </React.Fragment>
