f98d16056f5ea9fed1a280b8e4cff90dc4fdbfce
Blessing <blessvic2013@gmail.com>
2022-10-28 23:32:10 +0300

    updated

--- src/components/adviceGen.jsx
+++ src/components/adviceGen.jsx
@@ -6,7 +6,8 @@ class Generator extends Component {
       <React.Fragment>
         <div className="h-50 w-50 mx-auto d-block bg-primary p-3 mt-5">
           <h1>hello</h1>
-          <h1>hello</h1>
+          <h1>World</h1>
+
         </div>
       </React.Fragment>
     );
